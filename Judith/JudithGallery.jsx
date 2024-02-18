0d9e7fe614f81d4a4d45ff2ba615b39402fb1683
madgrv <mad.grv@gmail.com>
2024-02-18 18:00:49 +0000

    test code cleanup

--- Judith/JudithGallery.jsx
+++ Judith/JudithGallery.jsx
@@ -1,42 +1,29 @@
-123;
-
-<<<<<<< HEAD
-console.log('Helloooo Kangdani');
-
 import React from 'react';
 
 const MyComponent = () => {
-  return (
-    <div style={styles.container}>
-      <button style={styles.buttonTopLeft}>Top Left Button</button>
-      <button style={styles.buttonBottomLeft}>Bottom Left Button</button>
-    </div>
-  );
+	return (
+		<div style={styles.container}>
+			<button style={styles.buttonTopLeft}>Top Left Button</button>
+			<button style={styles.buttonBottomLeft}>Bottom Left Button</button>
+		</div>
+	);
 };
 
 const styles = {
-  container: {
-    backgroundColor: 'lightblue',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    padding: '20px',
-  },
-  buttonTopLeft: {
-    alignSelf: 'flex-start',
-  },
-  buttonBottomLeft: {
-    alignSelf: 'flex-start',
-  },
+	container: {
+		backgroundColor: 'lightblue',
+		height: '100vh',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		padding: '20px',
+	},
+	buttonTopLeft: {
+		alignSelf: 'flex-start',
+	},
+	buttonBottomLeft: {
+		alignSelf: 'flex-start',
+	},
 };
 
 export default MyComponent;
-=======
-console.log('Hello Kangdani');
-
-console.log('trying terminal git');
-let cacca = 'again!';
-
-console.log('HelloWorld');
->>>>>>> 429f2416c20bb2ec6c8bbe87534f7a53dce58b09
