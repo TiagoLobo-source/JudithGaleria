640e7a06ac83ffd8ad8170ef6cf75f4e8b377e81
madgrv <mad.grv@gmail.com>
2024-02-20 21:07:22 +0000

    Added resources links to constants.js file -Teo

--- judith-app/src/constants.js
+++ judith-app/src/constants.js
@@ -2,3 +2,9 @@ const styles = {
 	'--primary-color': 'grey',
 	'--secondary-color': 'purple',
 };
+
+// https://styled-components.com/docs/basics
+
+// https://www.joshwcomeau.com/css/styled-components/#
+
+// https://www.joshwcomeau.com/css/css-variables-for-react-devs/#introduction
