1c5430eef9b6bad6de363640f14c1650b151884d
madgrv <mad.grv@gmail.com>
2024-03-05 15:53:35 +0000

    Updated GlobalStyle.js  + updated and Renamed DesignSystem.js with Styles.js

---
+++ judith-app/src/Styles.js
@@ -0,0 +1,42 @@
+// Styles.js
+
+const Styles = {
+	fontFamily: 'Inter, sans-serif',
+	fontSize: '1em',
+	lineHeight: '19px',
+
+	textColor: 'white',
+	secondaryTextColor: 'gray',
+
+	infoCard: {
+		backgroundColor: 'hsla(0, 0%, 15%, 1)',
+		borderRadius: '0px 16px 16px 0px',
+	},
+
+	button: {
+		fontWeight: '400',
+		padding: '16px',
+		margin: '10px',
+		lineHeight: '19px',
+		backgroundColor: 'hsl(0, 2%, 8%)',
+		minHeight: '51px',
+		minWidth: '51px',
+		border: 'none',
+		// borderRadius: '71% 29% 65% 35% / 37% 56% 44% 63% ',
+		borderRadius: '40px',
+		cursor: 'pointer',
+
+		onHover: {
+			opacity: '0.5',
+			transition: '0.3s ease-in',
+		},
+
+		onActive: {
+			backgroundColor: '#8000ff',
+			opacity: '1',
+		},
+	},
+	// ... other design system properties
+};
+
+export default Styles;
