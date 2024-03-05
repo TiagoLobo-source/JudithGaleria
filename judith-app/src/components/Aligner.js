0044a4fec1684bc197c775c9f3fda3ad527ec969
madgrv <mad.grv@gmail.com>
2024-03-05 15:49:16 +0000

    Added Aligner wrapper component

---
+++ judith-app/src/components/Aligner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// A wrapper component to align any element to a
+// fixed position on screen, Use to wrap elements you want to have
+// a specific position and pass the position description as ex:
+//
+// <Aligner align={'top left'}>
+//      {children}
+// </Aligner>
+
+export default function Aligner({ align, children }) {
+	return <Wrapper align={align}>{children}</Wrapper>;
+}
+
+const Wrapper = styled.div`
+	position: absolute;
+	${({ align }) => (align ? alignStyles[align] : '')}
+`;
+
+const alignStyles = {
+	'top left': 'top: 0; left: 0;',
+	'top right': 'top: 0; right: 0;',
+	'bot left': 'bottom: 0; left: 0;',
+	'bot right': 'bottom: 0; right: 0;',
+	center: 'top: 50%; left: 50%;',
+};
