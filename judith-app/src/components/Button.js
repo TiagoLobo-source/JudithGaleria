97bca56bee5d23fdff62bcf9ac65dd2a0f3626fa
madgrv <mad.grv@gmail.com>
2024-03-05 15:50:13 +0000

    Created new Button component with style

---
+++ judith-app/src/components/Button.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Button({ link, onClick, children }) {
+	const handleClick = () => {
+		if (onClick) {
+			onClick();
+		}
+		// Redirect to the link if provided
+		if (link) {
+			window.location.href = link;
+		}
+	};
+
+	return <MyButton onClick={handleClick}>{children}</MyButton>;
+}
+
+const MyButton = styled.button`
+	/* Destructuring the 'theme' object inherited from the themeProvider 
+	in the props */
+	${({ theme }) => `
+    font-family: ${theme.fontFamily};
+    font-size: ${theme.fontSize};
+    line-height: ${theme.lineHeight};
+    padding: ${theme.button.padding};
+	margin: ${theme.button.margin};
+    min-height: ${theme.button.minHeight};
+    min-width: ${theme.button.minWidth};
+    background-color: ${theme.button.backgroundColor};
+    border: ${theme.button.borderRadius};
+    border-radius: ${theme.button.borderRadius};
+    color: ${theme.textColor};
+    cursor: ${theme.button.cursor};
+    opacity: ${theme.button.opacity};
+
+    &:hover {
+      opacity: ${theme.button.onHover.opacity};
+      transition: ${theme.button.onHover.transition};
+    }
+
+    &:active {
+      background-color: ${theme.button.onActive.backgroundColor};
+      opacity: ${theme.button.onActive.opacity};
+    }
+  `}
+`;
