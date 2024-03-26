5913f62cfb9a9a0df972c67970134ee2959fb755
Matteo <mad.grv@gmail.com>
2024-03-26 21:11:26 +0000

    Adding Appcopy with horizontal scroll

---
+++ judith-app/src/components/HorizontalScroll.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './HorizontalScroll.css'; 
+const CustomHorizontalScroll = ({ children }) => {
+  return <div className="scroll-container">{children}</div>;
+};
+
+export default CustomHorizontalScroll;
