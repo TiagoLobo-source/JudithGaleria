9da1c7803475bb7484eab702cf22e5a850cef346
Matteo <mad.grv@gmail.com>
2024-03-27 20:47:49 +0000

    Fixed horizontal scroll wrapper component code

--- judith-app/src/components/HorizontalScroll.js
+++ judith-app/src/components/HorizontalScroll.js
@@ -4,18 +4,18 @@ import styled from "styled-components"
 
 export default function HorizontalScrol ({ children })  {
 
-    // Convert vertical scrolling to horizontal scrolling for the HorizontalScroll component
-    useEffect(() => {
-      const horizontalScrollContainer = document.getElementById('horizontalscroll');
-      const handleWheel = (e) => {
-          if (e.deltaY === 0) return;
-          e.preventDefault();
-          horizontalScrollContainer.scrollLeft += e.deltaY + e.deltaX;
-      };
+  // Convert vertical scrolling to horizontal scrolling for the HorizontalScroll component
+  useEffect(() => {
+    const horizontalScrollContainer = document.getElementById('horizontalscroll');
+    const handleWheel = (e) => {
+        if (e.deltaY === 0) return;
+        e.preventDefault();
+        horizontalScrollContainer.scrollLeft += e.deltaY + e.deltaX;
+    };
 
-      horizontalScrollContainer.addEventListener('wheel', handleWheel);
+    horizontalScrollContainer.addEventListener('wheel', handleWheel);
 
-      return () => horizontalScrollContainer.removeEventListener('wheel', handleWheel);
+    return () => horizontalScrollContainer.removeEventListener('wheel', handleWheel);
   }, []);
 
 
@@ -26,11 +26,12 @@ export default function HorizontalScrol ({ children })  {
   )
 };
 
+
 const HorizontalWrapper = styled.div`
   display: flex;
   background-color: #151414;
-   overflow-x: auto;
-    width: 100vw; 
-    -webkit-overflow-scrolling: touch;
-    scrollbar-width: none;
+  overflow-x: auto;
+  width: 100vw; 
+  -webkit-overflow-scrolling: touch;
+  scrollbar-width: none;
 `
