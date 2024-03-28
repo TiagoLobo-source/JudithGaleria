e424e78dcc0a77692d031d296d9d0df3a8d9d402
Matteo <mad.grv@gmail.com>
2024-03-28 13:41:25 +0000

    Changed floating card animation and style

--- judith-app/src/components/FloatingCard.js
+++ judith-app/src/components/FloatingCard.js
@@ -17,17 +17,20 @@ export default function FloatingCard({ onClick, isShown }) {
 const FloatingCardContainer = styled.div`
 	${({ theme, isShown }) => `
 		background-color: ${theme.infoCard.backgroundColor};
-		border-radius: ${theme.infoCard.borderRadius};
+		border-radius: ${isShown ? '0% 80% 22% 80% / 108% 10% 68% 10%' : '5%'};
 		color: white;
 		position: absolute;
 		height: 98%;
 		width: 90%;
-		top: 1%;
+		top: 0.5%;
 		padding: 20px;
 		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-		transition: left 0.5s cubic-bezier(0.8, 0.1, 1, 0.2);
+		transition: left 0.5s cubic-bezier(0.8, 0.1, 1, 0.2), border-radius 0.5s ease-in;
 		left: ${isShown ? '-1.9%' : '-100%'};
 		z-index: 2;
 
 	`}
 `;
+
+
+// 		border-radius: ${theme.infoCard.borderRadius};
