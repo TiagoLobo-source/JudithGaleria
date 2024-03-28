650577212f15c49d0d843ce1ceb48a29def56ece
Matteo <mad.grv@gmail.com>
2024-03-28 13:56:57 +0000

    Code cleanup

--- judith-app/src/components/HorizontalScroll.js
+++ judith-app/src/components/HorizontalScroll.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import styled from "styled-components"
-// import './HorizontalScroll.css'; 
 
 export default function HorizontalScrol ({ children })  {
 
